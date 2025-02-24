64364df0b3ad23d4afccadb37c0ae1a17c1b3e05
MinhBui2k4 <130561119+MinhBui2k4@users.noreply.github.com>
2025-02-24 10:22:00 +0700

    first commit

---
+++ src/main/java/com/example/BuiVanMinh/BuiVanMinhApplication.java
@@ -0,0 +1,13 @@
+package com.example.BuiVanMinh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuiVanMinhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuiVanMinhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BuiVanMinh/BuiVanMinhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BuiVanMinh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuiVanMinhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
